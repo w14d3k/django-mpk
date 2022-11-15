36e2355e8d893843fd9d84a912c51c180764ed77
Aliaksandr Kholad <kholadaliaksandr@gmail.com>
2022-11-15 10:40:37 +0100

    dynamic site head change added

--- base/views.py
+++ base/views.py
@@ -26,6 +26,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            return redirect('/profile')
         else:
             print("Error")
     return render(request, "base/login.html", context)
